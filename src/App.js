647f67fe3daf161255dcaefa6c7a789c062711af
Rima Dagher <rima.t.dagher@gmail.com>
2025-06-07 15:28:21 -0400

    Initialized tailwind

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="text-4xl font-bold text-purple-600">Hello Tailwind!</h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/component/SearchBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SearchBar = () => {
+  // 1- User Input
+  // 2- Fetch data from unslplash
+  // 3-
+
+  const handleSubmit = () => {};
+
+  return (
+    <div onSubmit={handleSubmit}>
+      <input type="text" name="search"></input>
+    </div>
+  );
+};
+
+export default SearchBar;
